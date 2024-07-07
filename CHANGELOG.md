3574a16a81d7a0aad1ea1169fca7b8211a83676c
Fabien Meyer <fabien-meyer@outlook.com>
2024-07-07 21:20:18 +0200

    bump: version 0.0.1 → 0.0.2

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,1 +1,7 @@
-## Unreleased
+## v0.0.2 (2024-07-07)
+
+## v0.0.1 (2024-07-07)
+
+### Feat
+
+- initial commit
--- e_lims_core/__init__.py
+++ e_lims_core/__init__.py
@@ -2,4 +2,4 @@
 
 __author__ = """Fabien Meyer"""
 __email__ = 'fabien-meyer@outlook.com'
-__version__ = '0.0.1'
+__version__ = '0.0.2'
